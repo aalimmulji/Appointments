53fa3a61c2d9f5ffb4604d45a1d581638442f26b
Aalim Mulji <aalimmulji@gmail.com>
2019-05-12 19:05:03 -0400

    image upload completed

---
+++ Appointments/Controllers/updateProfileInfoController.swift
@@ -0,0 +1,30 @@
+//
+//  updateProfileInfoController.swift
+//  Appointments
+//
+//  Created by Aalim Mulji on 5/12/19.
+//  Copyright © 2019 Aalim Mulji. All rights reserved.
+//
+
+import UIKit
+
+class updateProfileInfoController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Appointments/View/Table Cells/ProfileInfoCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileInfoCell.swift
+//  Appointments
+//
+//  Created by Aalim Mulji on 5/11/19.
+//  Copyright © 2019 Aalim Mulji. All rights reserved.
+//
+
+import UIKit
+
+class ProfileInfoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
